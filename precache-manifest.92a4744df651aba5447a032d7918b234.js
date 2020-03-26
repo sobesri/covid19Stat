2cc33e670d770b705c3d224f590d2ea4949592f8
Sobedan <sridaran@calcey.com>
2020-03-26 21:08:28 +0530

    Updates

--- precache-manifest.92a4744df651aba5447a032d7918b234.js
+++ precache-manifest.92a4744df651aba5447a032d7918b234.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "05a67c65130eaa1d0e34531ffdcb8215",
+    "revision": "1fee2426ca13cb999c82420b8b24e581",
     "url": "/covid19Stat/index.html"
   },
   {
-    "revision": "e9187e655b43e9c46789",
+    "revision": "3c82d3ae4c6c6dcfe806",
     "url": "/covid19Stat/static/css/main.c27489cf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19Stat/static/js/2.3038bcc5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e9187e655b43e9c46789",
-    "url": "/covid19Stat/static/js/main.d2527203.chunk.js"
+    "revision": "3c82d3ae4c6c6dcfe806",
+    "url": "/covid19Stat/static/js/main.8a7610a8.chunk.js"
   },
   {
     "revision": "8f62dc1592b4c6873cf2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19Stat/precache-manifest.4f20c44291520c9991570a768788c2c6.js"
+  "/covid19Stat/precache-manifest.92a4744df651aba5447a032d7918b234.js"
 );
 
 self.addEventListener('message', (event) => {

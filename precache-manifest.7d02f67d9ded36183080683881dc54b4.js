ba950ab1d7c10695cf79cbd88951b97ba133873b
Sobedan <sridaran@calcey.com>
2020-04-02 20:38:56 +0530

    Updates

--- precache-manifest.7d02f67d9ded36183080683881dc54b4.js
+++ precache-manifest.7d02f67d9ded36183080683881dc54b4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e4e0f98af4dbe5cf57ba85c172296a0e",
+    "revision": "f7fa041b446dcdab8ac52d7f1a0b169d",
     "url": "/covid19Stat/index.html"
   },
   {
-    "revision": "207f415284d6589c4d44",
+    "revision": "1b973af0309aea02eb0f",
     "url": "/covid19Stat/static/css/main.6819f774.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19Stat/static/js/2.c0b27b31.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "207f415284d6589c4d44",
-    "url": "/covid19Stat/static/js/main.1ec910b4.chunk.js"
+    "revision": "1b973af0309aea02eb0f",
+    "url": "/covid19Stat/static/js/main.183d0346.chunk.js"
   },
   {
     "revision": "8f62dc1592b4c6873cf2",

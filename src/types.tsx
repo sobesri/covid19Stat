d6b4b809eda20890923a4427fe789f0c54e7f15b
Sobedan <sobe123sri@gmail.com>
2020-05-01 13:35:08 +0530

    timeline update

--- src/types.tsx
+++ src/types.tsx
@@ -144,5 +144,5 @@ export const LOCAL_TIMELINE: CaseSummary[] = [
   { date: new Date(2020, 3, 28), recovered: 134, deaths: 7, confirmed: 619 },
   { date: new Date(2020, 3, 29), recovered: 136, deaths: 7, confirmed: 649 },
   { date: new Date(2020, 3, 30), recovered: 154, deaths: 7, confirmed: 665 },
-  { date: new Date(2020, 4, 1), recovered: 154, deaths: 7, confirmed: 665 },
+  { date: new Date(2020, 4, 1), recovered: 157, deaths: 7, confirmed: 666 },
 ];

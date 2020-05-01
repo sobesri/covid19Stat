45babbd52618d1d9e18a163e82c14a27e4343ec7
Sobedan <sobe123sri@gmail.com>
2020-05-01 22:42:56 +0530

    Updates

--- precache-manifest.2de8394f5058cf260b9e0a907757a0c3.js
+++ precache-manifest.2de8394f5058cf260b9e0a907757a0c3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "384f0f6fa39332ca6a0fa2efe7d59789",
+    "revision": "6dccd3428180bfb32f67be771ee2df20",
     "url": "/covid19Stat/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19Stat/static/css/2.ad11eaf6.chunk.css"
   },
   {
-    "revision": "19ec1415afcf3a87411c",
+    "revision": "1426ee6f3e3547a851f2",
     "url": "/covid19Stat/static/css/main.c08d058a.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19Stat/static/js/2.f6d95132.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "19ec1415afcf3a87411c",
-    "url": "/covid19Stat/static/js/main.acb2b920.chunk.js"
+    "revision": "1426ee6f3e3547a851f2",
+    "url": "/covid19Stat/static/js/main.7beeb872.chunk.js"
   },
   {
     "revision": "8f62dc1592b4c6873cf2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19Stat/precache-manifest.41ef16bba01d60bcb7a99ab7cf47c7e3.js"
+  "/covid19Stat/precache-manifest.2de8394f5058cf260b9e0a907757a0c3.js"
 );
 
 self.addEventListener('message', (event) => {
